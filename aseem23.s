1528fbd13554ce5d27d4094528f31bf82dbb8e89
Samridhi Saini <83508209+SAMRIDHISAINII@users.noreply.github.com>
2022-05-26 21:43:40 +0530

    Create aseem23.s

---
+++ aseem23.s
@@ -0,0 +1,19 @@
+
+
+.data
+	pi : .float 3.14
+	valpi: .asciiz "The value of pi is : "
+.text 
+
+	li $v0,4
+	la $a0, valpi
+	syscall
+
+	l.s $f12, pi
+	li $v0,2
+	syscall
+exit: 
+	li $v0,10
+	syscall
+
+
